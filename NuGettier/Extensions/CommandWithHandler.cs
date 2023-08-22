7d9ac03629a048c05f39794927828189abdaec8f
Chris Helmich <kagekirin@gmail.com>
2023-08-22 19:43:13 +0900

    feature: implement Command.WithHandler() extension methods

---
+++ NuGettier/Extensions/CommandWithHandler.cs
@@ -0,0 +1,26 @@
+using System;
+using System.CommandLine;
+using System.CommandLine.NamingConventionBinder;
+using System.CommandLine.Invocation;
+using System.CommandLine.IO;
+using System.Reflection;
+
+namespace NuGettier;
+
+internal static class CommandWithHandlerExtension
+{
+    public static Command WithHandler(this Command command, ICommandHandler handler)
+    {
+        command.Handler = handler;
+        return command;
+    }
+
+    public static Command WithHandler(this Command command, string methodName)
+    {
+        var method = typeof(Program).GetMethod(
+            methodName,
+            BindingFlags.NonPublic | BindingFlags.Static
+        );
+        return command.WithHandler(CommandHandler.Create(method!));
+    }
+}
