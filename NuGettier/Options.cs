3f777fd6801a1367712a18c1160aad7b30e3d5bd
Chris Helmich <kagekirin@gmail.com>
2023-08-22 19:42:36 +0900

    feature: implement NuGettier program options

---
+++ NuGettier/Options.cs
@@ -0,0 +1,58 @@
+using System;
+using System.CommandLine;
+
+namespace NuGettier;
+
+public static partial class Program
+{
+    private static Option<bool> OutputJsonOption =
+        new(
+            aliases: new string[] { "--json", "-j" },
+            description: "whether to output result as JSON (for piping into `jq` etc)"
+        );
+
+    private static Option<bool> IncludePrereleaseOption =
+        new(
+            aliases: new string[] { "--preRelease", "-p" },
+            description: "whether to include prerelease versions"
+        );
+
+    private static Option<bool> IncludeDependenciesOption =
+        new(
+            aliases: new string[] { "--includeDependencies", "-i" },
+            description: "whether to include dependencies"
+        );
+
+    private static Option<bool> RetrieveLatestOption =
+        new(aliases: new string[] { "--latest", "-l" }, description: "fetch the latest version");
+
+    private static Option<string> SpecificVersionOption =
+        new(aliases: new string[] { "--version", "-v" }, description: "version to fetch");
+
+    private static Option<DirectoryInfo> OutputDirectoryOption =
+        new(
+            aliases: new string[] { "--outputDirectory", "-o" },
+            description: "directory to output files to"
+        )
+        {
+            IsRequired = true,
+        };
+
+    private static Option<Uri> SourceRepositoryOption =
+        new(
+            aliases: new string[] { "--source", "-s" },
+            description: "source NuGet repository to fetch from"
+        )
+        {
+            IsRequired = true,
+        };
+
+    private static Option<Uri> TargetRegistryOption =
+        new(
+            aliases: new string[] { "--target", "-t" },
+            description: "target NPM registry to publish to"
+        )
+        {
+            IsRequired = true,
+        };
+}
