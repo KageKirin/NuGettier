e4e654993ae582673b181d895cd52487fdd9e3a2
Chris Helmich <kagekirin@gmail.com>
2023-08-31 09:30:29 +0900

    refactor (Core.Context): gather method-common variables in Core.Context class

--- NuGettier.Core/Context.cs
+++ NuGettier.Core/Context.cs
@@ -1,3 +1,33 @@
+using System;
+using System.IO;
+using System.CommandLine;
+using NuGet.Common;
+using NuGet.Configuration;
+using NuGet.Frameworks;
+using NuGet.Packaging;
+using NuGet.Packaging.Core;
+using NuGet.Protocol;
+using NuGet.Protocol.Core.Types;
+using NuGet.Versioning;
+
 namespace NuGettier.Core;
 
-public partial class Context { }
+#nullable enable
+
+public partial class Context : IDisposable
+{
+    public Uri source { get; protected set; }
+    public SourceCacheContext cache { get; protected set; }
+    public SourceRepository repository { get; protected set; }
+    public IConsole console { get; set; }
+
+    public Context(Uri source, IConsole console)
+    {
+        this.source = source;
+        this.cache = new();
+        this.repository = Repository.Factory.GetCoreV3($"{source.ToString()}");
+        this.console = console;
+    }
+
+    public void Dispose() { }
+}
--- NuGettier.Upm/Context/Context.cs
+++ NuGettier.Upm/Context/Context.cs
@@ -1,5 +1,13 @@
+using System;
+using System.CommandLine;
 using NuGettier;
 
 namespace NuGettier.Upm;
 
-public partial class Context : Core.Context { }
+public partial class Context : Core.Context
+{
+    public Context(Uri source, IConsole console)
+        : base(source, console)
+    {
+    }
+}
