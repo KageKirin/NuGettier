e7ffe43a11ea84c48c81104ce42a9cd7ba95df17
Chris Helmich <kagekirin@gmail.com>
2023-09-20 12:46:15 +0900

    refactor (Get command): improve command output and printed information

--- NuGettier/CoreActions/Get.cs
+++ NuGettier/CoreActions/Get.cs
@@ -62,19 +62,67 @@ public static partial class Program
 
             Console.WriteLine($"ID: {nuspecReader.GetId()}");
             Console.WriteLine($"Title: {nuspecReader.GetTitle()}");
+            Console.WriteLine($"Summary: {nuspecReader.GetSummary()}");
+            Console.WriteLine($"Copyright: {nuspecReader.GetCopyright()}");
             Console.WriteLine($"Version: {nuspecReader.GetVersion()}");
             Console.WriteLine($"Tags: {nuspecReader.GetTags()}");
+            Console.WriteLine($"Readme: {nuspecReader.GetReadme()}");
+            Console.WriteLine($"Icon: {nuspecReader.GetIcon()}");
             Console.WriteLine($"Description: {nuspecReader.GetDescription()}");
             Console.WriteLine($"Authors: {nuspecReader.GetAuthors()}");
+            Console.WriteLine($"Owners: {nuspecReader.GetOwners()}");
+            Console.WriteLine($"ReleaseNotes: {nuspecReader.GetReleaseNotes()}");
+            Console.WriteLine($"Project Url: {nuspecReader.GetProjectUrl()}");
+            Console.WriteLine($"LicenseUrl: {nuspecReader.GetLicenseUrl()}");
+            Console.WriteLine($"Icon Url: {nuspecReader.GetIconUrl()}");
+            Console.WriteLine($"Language: {nuspecReader.GetLanguage()}");
+            Console.WriteLine($"LicenseMetadata: {nuspecReader.GetLicenseMetadata()}");
+            Console.WriteLine($"RepositoryMetadata: {nuspecReader.GetRepositoryMetadata()}");
 
             Console.WriteLine("Dependencies:");
             foreach (var dependencyGroup in nuspecReader.GetDependencyGroups())
             {
-                Console.WriteLine($" - {dependencyGroup.TargetFramework.GetShortFolderName()}");
+                Console.WriteLine($"- {dependencyGroup.TargetFramework.GetShortFolderName()}:");
                 foreach (var dependency in dependencyGroup.Packages)
                 {
-                    Console.WriteLine($"   > {dependency.Id} {dependency.VersionRange}");
+                    Console.WriteLine($"  - {dependency.ToString()}");
                 }
+            }
+
+            Console.WriteLine("Framework Assembly Groups:");
+            foreach (var group in nuspecReader.GetFrameworkAssemblyGroups())
+            {
+                Console.WriteLine($"- TargetFramework: {group.TargetFramework.ToString()}");
+                Console.WriteLine($"  HasEmptyFolder: {group.HasEmptyFolder}");
+                Console.WriteLine($"  Items:");
+                foreach (var item in group.Items)
+                {
+                    Console.WriteLine($"  - {item}");
+                }
+            }
+
+            Console.WriteLine("Reference Groups:");
+            foreach (var group in nuspecReader.GetReferenceGroups())
+            {
+                Console.WriteLine($"- TargetFramework: {group.TargetFramework.ToString()}");
+                Console.WriteLine($"  HasEmptyFolder: {group.HasEmptyFolder}");
+                Console.WriteLine($"  Items:");
+                foreach (var item in group.Items)
+                {
+                    Console.WriteLine($"  - {item}");
+                }
+            }
+
+            // not very interesting (empty)
+            Console.WriteLine("Content Files:");
+            foreach (var file in nuspecReader.GetContentFiles())
+            {
+                Console.WriteLine($"- file:");
+                Console.WriteLine($"  Include: {file.Include}");
+                Console.WriteLine($"  Exclude: {file.Exclude}");
+                Console.WriteLine($"  BuildAction: {file.BuildAction}");
+                Console.WriteLine($"  CopyToOutput: {file.CopyToOutput}");
+                Console.WriteLine($"  Flatten: {file.Flatten}");
             }
 
             Console.WriteLine("Files:");
