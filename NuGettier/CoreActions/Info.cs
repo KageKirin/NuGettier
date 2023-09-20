1b5f6ac782b7430739307c1c6c3dd492c6d370c0
Chris Helmich <kagekirin@gmail.com>
2023-09-20 12:45:50 +0900

    refactor (Info command): improve command output and printed information

--- NuGettier/CoreActions/Info.cs
+++ NuGettier/CoreActions/Info.cs
@@ -62,11 +62,39 @@ public static partial class Program
             }
             else
             {
+                Console.WriteLine($"Identity: {package.Identity}");
                 Console.WriteLine($"Version: {package.Identity.Version}");
                 Console.WriteLine($"Listed: {package.IsListed}");
-                Console.WriteLine($"Tags: {package.Tags}");
+                Console.WriteLine($"Title: {package.Title}");
+                Console.WriteLine($"Summary: {package.Summary}");
                 Console.WriteLine($"Description: {package.Description}");
                 Console.WriteLine($"Authors: {package.Authors}");
+                Console.WriteLine($"Owners: {package.Owners}");
+                Console.WriteLine($"Tags: {package.Tags}");
+                Console.WriteLine($"License: {package.LicenseMetadata?.License}");
+                Console.WriteLine($"Published: {package.Published}");
+                Console.WriteLine($"Prefix Reserved: {package.PrefixReserved}");
+                Console.WriteLine($"Project Url: {package.ProjectUrl}");
+                Console.WriteLine($"License Url: {package.LicenseUrl}");
+                Console.WriteLine(
+                    $"Require License Acceptance: {package.RequireLicenseAcceptance}"
+                );
+                Console.WriteLine($"Icon Url: {package.IconUrl}");
+                Console.WriteLine($"Readme Url: {package.ReadmeUrl}");
+                Console.WriteLine($"Report Abuse Url: {package.ReportAbuseUrl}");
+                Console.WriteLine($"Package Details Url: {package.PackageDetailsUrl}");
+                Console.WriteLine($"Dependencies:");
+                foreach (PackageDependencyGroup dependencyGroup in package.DependencySets)
+                {
+                    Console.WriteLine($"- {dependencyGroup.TargetFramework}:");
+                    foreach (PackageDependency dependency in dependencyGroup.Packages)
+                    {
+                        Console.WriteLine($"  - {dependency.ToString()}");
+                    }
+                }
+
+                //IEnumerable<PackageVulnerabilityMetadata> Vulnerabilities { get; }
+                //Task<IEnumerable<VersionInfo>> GetVersionsAsync();
             }
         }
 
