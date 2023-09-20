312ff0ecf8f399736dec71afcdaf0554b70d1aa8
Chris Helmich <kagekirin@gmail.com>
2023-09-20 14:39:02 +0900

    feature (NuGettier.Upm): implement extension methods for PackageArchiveReader

---
+++ NuGettier.Upm/NugetExtensions/PackageArchiveReaderExtension.cs
@@ -0,0 +1,53 @@
+using System.Linq;
+using NuGet.Packaging;
+
+namespace NuGettier.Upm;
+
+public static class PackageArchiveReaderExtension
+{
+    public static byte[] GetBytes(this PackageArchiveReader packageReader, string file)
+    {
+        using var stream = packageReader.GetStream(file);
+        using var ms = new MemoryStream();
+        stream.CopyTo(ms);
+        return ms.GetBuffer();
+    }
+
+    public static IEnumerable<string> GetAssemblyFrameworks(this PackageArchiveReader packageReader)
+    {
+        return packageReader
+            .GetFiles("lib")
+            .Select(f => f.Replace("lib/", string.Empty))
+            .Select(f => Path.GetPathRoot(f))
+            .Distinct();
+    }
+
+    public static string? GetPreferredFramework(
+        this PackageArchiveReader packageReader,
+        IEnumerable<string> frameworks
+    )
+    {
+        var assemblyFrameworks = packageReader.GetAssemblyFrameworks();
+
+        foreach (var framework in frameworks)
+        {
+            if (assemblyFrameworks.Contains(framework))
+                return framework;
+        }
+
+        return null;
+    }
+
+    public static TarGz.TarDictionary GetFrameworkFiles(
+        this PackageArchiveReader packageReader,
+        string framework
+    )
+    {
+        return new TarGz.TarDictionary(
+            packageReader
+                .GetFiles("lib")
+                .Where(f => Path.GetDirectoryName(f) == Path.Join("lib", framework))
+                .ToDictionary(f => Path.GetFileName(f), f => packageReader.GetBytes(f))
+        );
+    }
+}
