062c6fb9c1edf914f304ffd78905e200343763cc
Chris Helmich <kagekirin@gmail.com>
2023-09-21 12:46:42 +0900

    feature (NuGettier.Upm): implement StringStringDictionary ctors

--- NuGettier.Upm/PackageJson/PackageJson.cs
+++ NuGettier.Upm/PackageJson/PackageJson.cs
@@ -6,7 +6,20 @@ namespace NuGettier.Upm;
 
 public class PackageJson
 {
-    public sealed class StringStringDictionary : Dictionary<string, string> { }
+    public sealed class StringStringDictionary : Dictionary<string, string>
+    {
+        public StringStringDictionary()
+            : base() { }
+
+        public StringStringDictionary(StringStringDictionary reference)
+            : this(reference as Dictionary<string, string>) { }
+
+        public StringStringDictionary(Dictionary<string, string> reference)
+            : base(reference) { }
+
+        public StringStringDictionary(IEnumerable<KeyValuePair<string, string>> reference)
+            : base(reference) { }
+    }
 
     public string Name = String.Empty;
     public string name
