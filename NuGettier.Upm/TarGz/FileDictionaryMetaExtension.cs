b6208f43674516fb4234ad364b947e9694cade8d
Chris Helmich <kagekirin@gmail.com>
2023-09-21 15:12:18 +0900

    refactor (NuGettier.Upm): generate meta files for folders

--- NuGettier.Upm/TarGz/FileDictionaryMetaExtension.cs
+++ NuGettier.Upm/TarGz/FileDictionaryMetaExtension.cs
@@ -8,9 +8,26 @@ public static class FileDictionaryMetaExtension
 {
     public static void AddMetaFiles(this FileDictionary fileDictionary, string seed)
     {
+        var files = fileDictionary.Keys.ToList(); //< make a temp copy to avoid modifying the collection
+
+        // gather folders: Unity requires .meta files for each included folder as well
+        var folders = files
+            .SelectMany(f =>
+            {
+                var parents = new List<string>();
+                var p = Path.GetDirectoryName(f);
+                while (!string.IsNullOrEmpty(p))
+                {
+                    parents.Add(p);
+                    p = Path.GetDirectoryName(p);
+                }
+                return parents;
+            })
+            .Distinct();
+
         fileDictionary.AddRange(
-            fileDictionary.Keys
-                .ToList() //< make a temp copy to avoid modifying the collection
+            files
+                .Concat(folders) //< files AND folders
                 .Where(file => Path.GetExtension(file) != @".meta")
                 .Select(
                     file =>
