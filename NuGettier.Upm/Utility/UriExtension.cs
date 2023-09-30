2d8fac1c9c75992ea9b4db3bffe7999b222a0dc3
Chris Helmich <kagekirin@gmail.com>
2023-09-30 10:35:39 +0900

    feature (NuGettier.Upm): implement Uri extension method to retrive the schemeless Uri as required by 'npm'

---
+++ NuGettier.Upm/Utility/UriExtension.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace NuGettier.Upm;
+
+public static class UriExtension
+{
+    /// <summary>
+    /// allows to get the schemeless Uri as required by `npm`
+    /// https://my-awesome-server/npmapi -> my-awesome-server/npmapi
+    /// </summary>
+    /// <returns>Uri.AbsoluteUri without the scheme</returns>
+    public static string SchemelessUri(this Uri uri)
+    {
+        return uri.AbsoluteUri.Replace($"{uri.Scheme}//", "");
+    }
+}
