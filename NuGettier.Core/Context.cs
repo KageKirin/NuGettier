fc13f0501b6a8d7cb3b739735319c5935b560b9a
Chris Helmich <kagekirin@gmail.com>
2023-10-24 23:06:33 +0900

    refactor (NuGettier.Core): rename Context.source -> .Source

--- NuGettier.Core/Context.cs
+++ NuGettier.Core/Context.cs
@@ -16,7 +16,7 @@ namespace NuGettier.Core;
 
 public partial class Context : IDisposable
 {
-    public Uri source { get; protected set; }
+    public Uri Source { get; protected set; }
     public SourceCacheContext cache { get; protected set; }
     public SourceRepository repository { get; protected set; }
     public IConsole console { get; set; }
@@ -25,7 +25,7 @@ public partial class Context : IDisposable
     {
         console.WriteLine($"username: {username}");
         console.WriteLine($"password: {password}");
-        this.source = source;
+        this.Source = source;
         this.cache = new();
         PackageSource packageSource =
             new(source.ToString(), @"source")
