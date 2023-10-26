06a1ca686ef2ad31dfe7ad300088f1152f27dedb
Chris Helmich <kagekirin@gmail.com>
2023-10-26 14:42:02 +0900

    refactor (NuGettier.Core): adapt Core.Context property 'SourceRepository Repository' to 'IEnumerable<SourceRepository> Repositories'

--- NuGettier.Core/Context.cs
+++ NuGettier.Core/Context.cs
@@ -18,40 +18,43 @@ public partial class Context : IDisposable
 {
     public IEnumerable<Uri> Sources { get; protected set; }
     public SourceCacheContext Cache { get; protected set; }
-    public SourceRepository Repository { get; protected set; }
+    public IEnumerable<SourceRepository> Repositories { get; protected set; }
     public IConsole Console { get; set; }
 
     public Context(IEnumerable<Uri> sources, IConsole console)
     {
-        string? username = null;
-        string? password = null;
-        if (!string.IsNullOrEmpty(source.UserInfo))
-        {
-            var userInfo = source.UserInfo.Split(':');
-            username = userInfo[0];
-            password = userInfo[1];
-        }
-        console.WriteLine($"username: {username}");
-        console.WriteLine($"password: {password}");
-        this.Source = source;
-        this.Cache = new();
-        PackageSource packageSource =
-            new(source.ToString(), @"source")
-            {
-                Credentials =
-                    (username is not null && password is not null)
-                        ? PackageSourceCredential.FromUserInput(
-                            source: @"source",
-                            username: username,
-                            password: password,
-                            storePasswordInClearText: true,
-                            validAuthenticationTypesText: null
-                        )
-                        : null,
-            };
+        Console = console;
+        Sources = sources;
+        Cache = new();
 
-        this.Repository = NuGet.Protocol.Core.Types.Repository.Factory.GetCoreV3(packageSource);
-        this.Console = console;
+        Repositories = Sources.Select(source =>
+        {
+            string? username = null;
+            string? password = null;
+            if (!string.IsNullOrEmpty(source.UserInfo))
+            {
+                var userInfo = source.UserInfo.Split(':');
+                username = userInfo[0];
+                password = userInfo[1];
+            }
+            console.WriteLine($"username: {username}");
+            console.WriteLine($"password: {password}");
+            PackageSource packageSource =
+                new(source.ToString(), source.Authority)
+                {
+                    Credentials =
+                        (username is not null && password is not null)
+                            ? PackageSourceCredential.FromUserInput(
+                                source: source.Authority,
+                                username: username,
+                                password: password,
+                                storePasswordInClearText: true,
+                                validAuthenticationTypesText: null
+                            )
+                            : null,
+                };
+            return NuGet.Protocol.Core.Types.Repository.Factory.GetCoreV3(packageSource);
+        });
     }
 
     public void Dispose() { }
