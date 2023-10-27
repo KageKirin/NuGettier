8ed061c67e5aa7a7fa7866ba7efbe7151c7860d7
Chris Helmich <kagekirin@gmail.com>
2023-10-27 14:56:11 +0900

    feature (NuGettier): load configuration (.netconfig) via ConfigurationBuilder at program start

--- NuGettier/Program.cs
+++ NuGettier/Program.cs
@@ -7,13 +7,22 @@ using System.CommandLine;
 using System.CommandLine.NamingConventionBinder;
 using System.CommandLine.Invocation;
 using System.CommandLine.IO;
+using DotNetConfig;
+using Microsoft.Extensions.Configuration;
+
+#nullable enable
 
 namespace NuGettier;
 
 public static partial class Program
 {
+    private static IConfigurationRoot? Configuration;
     static async Task<int> Main(string[] args)
     {
+        var Configuration = new ConfigurationBuilder()
+            .AddDotNetConfig()
+            .Build();
+
         var cmd = new RootCommand
         { //
             SearchCommand,
