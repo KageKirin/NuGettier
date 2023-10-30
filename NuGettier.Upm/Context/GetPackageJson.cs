a7cf6292c1f1bbb665288f3f918f88bf4eb1dfc4
Chris Helmich <kagekirin@gmail.com>
2023-10-30 13:35:29 +0900

    feature (NuGettier.Upm): implement Upm.Context.GetPackageJson()
    note: copy-pasta adapted from current 'upm info' command

---
+++ NuGettier.Upm/Context/GetPackageJson.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Text.Json;
+using System.CommandLine;
+using NuGet.Common;
+using NuGet.Configuration;
+using NuGet.Frameworks;
+using NuGet.Packaging;
+using NuGet.Packaging.Core;
+using NuGet.Protocol;
+using NuGet.Protocol.Core.Types;
+using NuGet.Versioning;
+
+namespace NuGettier.Upm;
+
+#nullable enable
+
+public partial class Context
+{
+    public async Task<PackageJson?> GetPackageJson(
+        string packageName,
+        bool preRelease,
+        bool latest,
+        string? version,
+        CancellationToken cancellationToken
+    )
+    {
+        var package = await GetPackageInformation(
+            packageName: packageName,
+            preRelease: preRelease,
+            latest: latest,
+            version: version,
+            cancellationToken: cancellationToken
+        );
+
+        if (package == null)
+            return null;
+
+        return PatchPackageJson(package.ToPackageJson());
+    }
+}
