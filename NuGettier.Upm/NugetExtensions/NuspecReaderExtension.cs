3d52a9b1687b6de7f9291a0dc01ab5c1b78ffd18
Chris Helmich <kagekirin@gmail.com>
2023-10-31 22:51:30 +0900

    refactor (NuGettier.Upm): remove extension method NuspecReader.GetUpmPackageName()
    reason: unreferenced => not used

--- NuGettier.Upm/NugetExtensions/NuspecReaderExtension.cs
+++ NuGettier.Upm/NugetExtensions/NuspecReaderExtension.cs
@@ -9,14 +9,6 @@ namespace NuGettier.Upm;
 
 public static class NuspecReaderExtension
 {
-    public static string GetUpmPackageName(
-        this NuspecReader nuspecReader,
-        IEnumerable<Context.PackageRule> packageRules
-    )
-    {
-        return GetUpmPackageName(nuspecReader.GetAuthors(), nuspecReader.GetId(), packageRules);
-    }
-
     public static string GetUpmPackageName(string author, string id, IEnumerable<Context.PackageRule> packageRules)
     {
         return packageRules.Where(p => p.Id == id && !string.IsNullOrEmpty(p.Name)).Select(p => p.Name).FirstOrDefault()
