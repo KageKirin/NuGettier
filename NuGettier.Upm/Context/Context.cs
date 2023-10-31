bf2d71027da35a75e9228013bea260f52642f122
Christian Helmich <kagekirin@gmail.com>
2023-10-31 13:39:00 +0900

    feature (NuGettier.Upm): implement Upm.Context.PackageRule and their handling (feature/packagerules) [#60]
    - feature (NuGettier.Upm): implement Upm.Context.PackageRule
    - feature (NuGettier.Upm): add PackageRules to Upm.Context
    - feature (NuGettier.Upm): load PackageRules from .netconfig in Upm.Context ctor
    - refactor (NuGettier.Upm): change Upm.Context.PackageRule to record class
    - feature (NuGettier.Upm): add DefaultPackageRule to Upm.Context
    - refactor (NuGettier.Upm): extend Upm.Context.PackageRule with Framework field
    - refactor (NuGettier.Upm): use named parameters in DefaultPackageRule creation
    - refactor (NuGettier.Upm): move Upm.Context static globals to their own source
    
    - feature (NuGettier.Upm): implement Upm.Context.PackageRule
    - feature (NuGettier.Upm): add PackageRules to Upm.Context
    - feature (NuGettier.Upm): load PackageRules from .netconfig in Upm.Context ctor
    - refactor (NuGettier.Upm): change Upm.Context.PackageRule to record class
    - feature (NuGettier.Upm): add DefaultPackageRule to Upm.Context
    - refactor (NuGettier.Upm): extend Upm.Context.PackageRule with Framework field
    - refactor (NuGettier.Upm): use named parameters in DefaultPackageRule creation
    - refactor (NuGettier.Upm): move Upm.Context static globals to their own source

--- NuGettier.Upm/Context/Context.cs
+++ NuGettier.Upm/Context/Context.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Linq;
+using System.Collections.Generic;
 using System.CommandLine;
 using NuGettier;
 using Microsoft.Extensions.Configuration;
@@ -7,17 +9,42 @@ namespace NuGettier.Upm;
 
 public partial class Context : Core.Context
 {
+    public record class PackageRule(string Id, bool IsIgnored, string Name, string Version, string Framework);
+
     public Uri Target { get; protected set; }
+    public IEnumerable<PackageRule> PackageRules { get; protected set; }
 
     public Context(IConfigurationRoot configuration, IEnumerable<Uri> sources, Uri target, IConsole console)
         : base(configuration, sources, console)
     {
         this.Target = target;
+
+        this.PackageRules = Configuration
+            .GetSection(@"package")
+            .GetChildren()
+            .Select(packageSection =>
+            {
+                console.WriteLine($"package key: {packageSection.Key}");
+                return new PackageRule(
+                    Id: packageSection.Key,
+                    IsIgnored: packageSection.GetValue<bool>("ignore"),
+                    Name: packageSection.GetValue<string>("name") ?? string.Empty,
+                    Version: packageSection.GetValue<string>("version") ?? string.Empty,
+                    Framework: packageSection.GetValue<string>("framework") ?? string.Empty
+                );
+            })
+            .Distinct();
+
+        foreach (var p in PackageRules)
+        {
+            console.WriteLine($"{p}");
+        }
     }
 
     public Context(Context other)
         : base(other)
     {
         Target = other.Target;
+        PackageRules = other.PackageRules;
     }
 }
