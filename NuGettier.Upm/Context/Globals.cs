45b84475d2e7b9da7ebfc9340c7e16db5d190aff
Chris Helmich <kagekirin@gmail.com>
2023-11-02 10:10:29 +0900

    feature (NuGettier.Upm): add LUT to match .NET framework against Unity version for max. compatibility

--- NuGettier.Upm/Context/Globals.cs
+++ NuGettier.Upm/Context/Globals.cs
@@ -10,6 +10,13 @@ namespace NuGettier.Upm;
 
 public partial class Context
 {
+    public static readonly IDictionary<string, string> DefaultSupportedFrameworks = new Dictionary<string, string>()
+    {
+        { "netstandard2.1", "2021.3" },
+        { "netstandard2.0", "2018.1" },
+        { "net462", "2017.4" },
+    };
+
     public static readonly string[] DefaultFrameworks = new[]
     {
         // by order of preference
