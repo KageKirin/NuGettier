facd0ac055ef47e48de3529f9ee7ca622320e857
Chris Helmich <kagekirin@gmail.com>
2023-11-10 21:38:36 +0900

    refactor: promote PackageRule property from NuGettier.Upm.Context to NuGettier.Core.Context

--- NuGettier.Core/Context/Context.cs
+++ NuGettier.Core/Context/Context.cs
@@ -30,6 +30,7 @@ public partial class Context : IDisposable
     public IEnumerable<SourceRepository> Repositories { get; protected set; }
     public IConsole Console { get; protected set; }
     public BuildInfo Build { get; protected set; }
+    public IEnumerable<PackageRule> PackageRules { get; protected set; }
 
     public Context(IConfigurationRoot configuration, IEnumerable<Uri> sources, IConsole console)
     {
@@ -102,6 +103,27 @@ public partial class Context : IDisposable
                 };
             return NuGet.Protocol.Core.Types.Repository.Factory.GetCoreV3(packageSource);
         });
+
+        this.PackageRules = Configuration
+            .GetSection(@"package")
+            .GetChildren()
+            .Select(packageSection =>
+            {
+                console.WriteLine($"package key: {packageSection.Key}");
+                return new PackageRule(
+                    Id: packageSection.Key,
+                    IsIgnored: packageSection.GetValue<bool>("ignore"),
+                    Name: packageSection.GetValue<string>("name") ?? string.Empty,
+                    Version: packageSection.GetValue<string>("version") ?? string.Empty,
+                    Framework: packageSection.GetValue<string>("framework") ?? string.Empty
+                );
+            })
+            .Distinct();
+
+        foreach (var p in PackageRules)
+        {
+            console.WriteLine($"{p}");
+        }
     }
 
     public Context(Context other)
@@ -112,6 +134,7 @@ public partial class Context : IDisposable
         Cache = other.Cache;
         Build = other.Build;
         Repositories = other.Repositories;
+        PackageRules = other.PackageRules;
     }
 
     public void Dispose() { }
