d4b6196ac8a4216a45c8c103a1943e0b8d35d466
Chris Helmich <kagekirin@gmail.com>
2023-12-15 15:16:32 +0900

    feature (NuGettier.Amalgamate): implement override method Amalgamate.Context.NuGettier.PackUpmPackage()

---
+++ NuGettier.Amalgamate/Context/PackUpmPackage.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Text;
+using System.Text.Json;
+using System.Reflection;
+using System.CommandLine;
+using NuGet.Common;
+using NuGet.Configuration;
+using NuGet.Frameworks;
+using NuGet.Packaging;
+using NuGet.Packaging.Core;
+using NuGet.Protocol;
+using NuGet.Protocol.Core.Types;
+using NuGet.Versioning;
+using ICSharpCode.SharpZipLib.Tar;
+using ICSharpCode.SharpZipLib.GZip;
+using NuGettier.Upm.TarGz;
+using Xunit;
+
+namespace NuGettier.Amalgamate;
+
+using NuRepository = NuGet.Protocol.Core.Types.Repository;
+
+public partial class Context
+{
+    public override async Task<Tuple<string, FileDictionary>?> PackUpmPackage(
+        string packageName,
+        bool preRelease,
+        bool latest,
+        string? version,
+        string? prereleaseSuffix,
+        string? buildmetaSuffix,
+        CancellationToken cancellationToken
+    )
+    {
+        return await base.PackUpmPackage(
+            packageName: packageName,
+            preRelease: preRelease,
+            latest: latest,
+            version: version,
+            prereleaseSuffix: prereleaseSuffix,
+            buildmetaSuffix: buildmetaSuffix,
+            cancellationToken: cancellationToken
+        );
+    }
+}
