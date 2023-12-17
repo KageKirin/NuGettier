8737a91e6ae506ee84714c448392188f936cb01b
Chris Helmich <kagekirin@gmail.com>
2023-12-17 22:01:10 +0900

    refactor (NuGettier): rename Arguments parameter packageName -> packageId

--- NuGettier/Arguments.cs
+++ NuGettier/Arguments.cs
@@ -5,6 +5,6 @@ namespace NuGettier;
 
 public static partial class Program
 {
-    private static Argument<string> PackageIdArgument = new("packageName", "complete and exact package id");
+    private static Argument<string> PackageIdArgument = new("packageId", "complete and exact package id");
     private static Argument<string> SearchTermArgument = new("searchTerm", "package name-ish or search term");
 }
