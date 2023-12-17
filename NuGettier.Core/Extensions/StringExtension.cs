0df18f6a8f6488ee766d74195c7c9207aa0c1113
Chris Helmich <kagekirin@gmail.com>
2023-12-17 23:08:51 +0900

    feature (NuGettier.Core): implement string.SplitPackageIdVersion() extension method

--- NuGettier.Core/Extensions/StringExtension.cs
+++ NuGettier.Core/Extensions/StringExtension.cs
@@ -9,4 +9,29 @@ public static class StringExtension
     {
         return new Regex(self.Replace(@".", @"\.").Replace(@"%", @".?").Replace(@"*", @".*"), RegexOptions.Compiled);
     }
+
+    public static void SplitPackageIdVersion(
+        this string self,
+        out string packageId,
+        out string? version,
+        out bool latest
+    )
+    {
+        var parts = self.Split(@"@");
+        if (parts.Length >= 1)
+            packageId = parts[0];
+        else
+            packageId = self;
+
+        version = null;
+        latest = false;
+
+        if (parts.Length >= 2)
+        {
+            latest = parts[1] == "latest";
+
+            if (!latest)
+                version = parts[1];
+        }
+    }
 }
