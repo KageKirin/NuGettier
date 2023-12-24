8ec4003940d667b931834b7e0a7ef4ad27704d7c
Chris Helmich <kagekirin@gmail.com>
2023-12-25 01:46:11 +0900

    feature (NuGettier.Amalgamate): implement Amalgamate.Context.PatchPackageDependencies() override
    feature: apply (!)PackageRule.IsExcluded to keep excluded, i.e. non-amalgamated packages as dependencies

--- NuGettier.Amalgamate/Context/Utility.cs
+++ NuGettier.Amalgamate/Context/Utility.cs
@@ -18,4 +18,16 @@ public partial class Context
     {
         return $"{base.PatchPackageId(packageId)}.amalgamate";
     }
+
+    protected override IDictionary<string, string> PatchPackageDependencies(IDictionary<string, string> dependencies)
+    {
+        return dependencies
+            .Where(d => GetPackageRule(d.Key).IsIgnored == false) //< filter: remove 'ignored' dependencies
+            .Where(d => GetPackageRule(d.Key).IsExcluded == true) //< filter: 'excluded' dependencies are not amalgamated
+            .ToDictionary(
+                //< calling the base method allows to override PatchPackageId, PatchPackageVersion without affecting the dependencies
+                d => base.PatchPackageId(d.Key),
+                d => base.PatchPackageVersion(d.Key, d.Value)
+            );
+    }
 }
