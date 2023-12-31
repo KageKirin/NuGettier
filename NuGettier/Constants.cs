d284bf8b5cf1b66a306a3ca14823c79a3a7c04ea
Christian Helmich <kagekirin@gmail.com>
2023-12-31 22:45:45 +0900

    refactor (NuGettier): use constant string for 'defaults' section and value keys (refactor/use-constants-for-config-keys) [#119]
    - refactor (NuGettier): use constant string for 'defaults' section key
    - refactor (NuGettier): use constant string for 'output-direction' value key
    - refactor (NuGettier): use constant string for 'target' value key
    - refactor (NuGettier): use constant string for 'unity' value key
    - refactor (NuGettier): use constant string for 'prerelease-suffix' value key
    - refactor (NuGettier): use constant string for 'buildmeta-suffix' value key
    
    - refactor (NuGettier): use constant string for 'defaults' section key
    - refactor (NuGettier): use constant string for 'output-direction' value key
    - refactor (NuGettier): use constant string for 'target' value key
    - refactor (NuGettier): use constant string for 'unity' value key
    - refactor (NuGettier): use constant string for 'prerelease-suffix' value key
    - refactor (NuGettier): use constant string for 'buildmeta-suffix' value key

---
+++ NuGettier/Constants.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace NuGettier;
+
+public static partial class Program
+{
+    const string kDefaultsSection = "defaults";
+    const string kOutputDirectoryKey = "output-directory";
+    const string kTargetKey = "target";
+    const string kUnityKey = "unity";
+    const string kPrereleaseSuffixKey = "prerelease-suffix";
+    const string kBuildmetaSuffixKey = "buildmeta-suffix";
+}
