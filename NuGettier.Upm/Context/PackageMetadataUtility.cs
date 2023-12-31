ddea86d19d56c7f2df48bb51700be0c22669671f
Chris Helmich <kagekirin@gmail.com>
2023-12-31 23:14:53 +0900

    refactor (NuGettier.Upm): reorder usings in PackageMetadataUtility.cs

--- NuGettier.Upm/Context/PackageMetadataUtility.cs
+++ NuGettier.Upm/Context/PackageMetadataUtility.cs
@@ -1,16 +1,16 @@
 using System;
-using System.Linq;
 using System.Collections.Generic;
+using System.Linq;
 using System.Net;
 using System.Reflection;
 using System.Text.RegularExpressions;
+using HandlebarsDotNet;
 using NuGet.Configuration;
 using NuGet.Frameworks;
 using NuGet.Packaging;
 using NuGet.Packaging.Licenses;
 using NuGet.Protocol.Core.Types;
 using NuGet.Shared;
-using HandlebarsDotNet;
 using Xunit;
 
 namespace NuGettier.Upm;
