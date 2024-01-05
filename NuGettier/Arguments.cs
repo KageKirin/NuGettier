b4062e273a56c5b5bd85ca3d72ccde67a1ae8679
Chris Helmich <kagekirin@gmail.com>
2024-01-05 11:41:21 +0900

    refactor (NuGettier): change all Argument<> instances in Program into regular, non-static properties

--- NuGettier/Arguments.cs
+++ NuGettier/Arguments.cs
@@ -5,8 +5,8 @@ namespace NuGettier;
 
 public partial class Program
 {
-    private static Argument<string> PackageIdArgument = new("packageId", "complete and exact package id");
-    private static Argument<string> PackageIdVersionArgument =
+    private Argument<string> PackageIdArgument = new("packageId", "complete and exact package id");
+    private Argument<string> PackageIdVersionArgument =
         new("packageIdVersion", "package.id[@version | @latest]");
-    private static Argument<string> SearchTermArgument = new("searchTerm", "package id-ish or search term");
+    private Argument<string> SearchTermArgument = new("searchTerm", "package id-ish or search term");
 }
