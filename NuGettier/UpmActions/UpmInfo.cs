c25ccd0bbfff121fdfa73557d39ea23148a1d633
Chris Helmich <kagekirin@gmail.com>
2024-01-16 12:30:20 +0900

    refactor (NuGettier): add trace log to UpmInfo()

--- NuGettier/UpmActions/UpmInfo.cs
+++ NuGettier/UpmActions/UpmInfo.cs
@@ -56,6 +56,7 @@ public partial class Program
         CancellationToken cancellationToken
     )
     {
+        Logger.LogTrace("entered {0} command handler", "UpmInfo");
         Assert.NotNull(Configuration);
         using var context = new Upm.Context(
             configuration: Configuration!,
@@ -104,6 +105,7 @@ public partial class Program
             }
         }
 
+        Logger.LogTrace("exit {0} command handler without error", "UpmInfo");
         return 0;
     }
 }
