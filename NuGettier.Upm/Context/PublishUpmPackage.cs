b9f5072288e66632231693ac49e52c6fe6dca442
Christian Helmich <kagekirin@gmail.com>
2024-01-16 23:00:42 +0900

    refactor (NuGettier.Upm): add logs to Upm.Context.PublishUpmPackage() (refactor/extend-logs-in-upm-publish) [#149]
    - refactor (NuGettier.Upm): add error logs to Upm.Context.PublishUpmPackage()
    - refactor (NuGettier.Upm): add debug logs to Upm.Context.PublishUpmPackage()
    - refactor (NuGettier.Upm): add trace logs to Upm.Context.PublishUpmPackage()
    
    - refactor (NuGettier.Upm): add error logs to Upm.Context.PublishUpmPackage()
    - refactor (NuGettier.Upm): add debug logs to Upm.Context.PublishUpmPackage()
    - refactor (NuGettier.Upm): add trace logs to Upm.Context.PublishUpmPackage()

--- NuGettier.Upm/Context/PublishUpmPackage.cs
+++ NuGettier.Upm/Context/PublishUpmPackage.cs
@@ -39,6 +39,7 @@ public partial class Context
     )
     {
         using var scope = Logger.TraceLocation().BeginScope(this.__METHOD__());
+        Logger.LogTrace("creating UPM package for {0}", packageIdVersion);
         var tuple = await PackUpmPackage(
             packageIdVersion: packageIdVersion,
             preRelease: preRelease,
@@ -48,18 +49,25 @@ public partial class Context
         );
 
         if (tuple == null)
+        {
+            Logger.TraceLocation().LogError("failed to pack UPM package for {0}", packageIdVersion);
             return -1;
+        }
 
         var (packageIdentifier, package) = tuple!;
         using (package)
         {
             int exitCode = -2;
             var tempDir = Path.Join(Path.GetTempPath(), Path.GetRandomFileName());
+            Logger.LogDebug("temp working directory: {0}", tempDir);
+
             var packageFile = $"{packageIdentifier}.tgz";
+            Logger.LogTrace("writing package file: {0}", Path.Join(tempDir, packageFile));
             await package.WriteToTarGzAsync(Path.Join(tempDir, packageFile));
 
             if (token != null)
             {
+                Logger.LogTrace("writing .npmrc to {0}", tempDir);
                 using var npmrcWriter = new StreamWriter(File.OpenWrite(Path.Join(tempDir, ".npmrc")));
 
                 // format is "//${schemeless_registry}/:_authToken=${token}"
@@ -67,11 +75,13 @@ public partial class Context
             }
             else if (npmrc != null)
             {
+                Logger.LogTrace("copying .npmrc to {0}", tempDir);
                 File.Copy(npmrc, Path.Join(tempDir, ".npmrc"));
             }
 
             try
             {
+                Logger.LogTrace("creating process for `npm publish`");
                 using var process = new System.Diagnostics.Process();
                 process.StartInfo.UseShellExecute = false;
                 process.StartInfo.CreateNoWindow = true;
@@ -90,11 +100,25 @@ public partial class Context
                     $"--access {packageAccessLevel.ToString().ToLowerInvariant()}"
                 );
 
+                Logger.LogDebug(
+                    "starting process for `{0} {1}`",
+                    process.StartInfo.FileName,
+                    process.StartInfo.Arguments
+                );
                 process.Start();
+
+                Logger.LogDebug("started process for {0} (waiting until termination to proceed)", process.Id);
                 await process.WaitForExitAsync(cancellationToken);
+                Logger.LogDebug("process {0} has terminated with exit code {1}", process.Id, process.ExitCode);
                 exitCode = process.ExitCode;
+
+                Logger.LogTrace("reading STDOUT");
                 var stdout = await process.StandardOutput.ReadToEndAsync(cancellationToken);
+                Logger.LogTrace("read STDOUT: '{0}'", stdout);
+
+                Logger.LogTrace("reading STDERR");
                 var stderr = await process.StandardError.ReadToEndAsync(cancellationToken);
+                Logger.LogTrace("read STDERR: '{0}'", stderr);
 
                 if (!string.IsNullOrEmpty(stdout))
                     Logger.LogInformation($"NPM: {stdout}");
@@ -107,6 +131,7 @@ public partial class Context
                 Logger.LogError($"NPM: {e.Message}");
             }
 
+            Logger.LogTrace("deleting temp working directory: {0}", tempDir);
             System.IO.Directory.Delete(tempDir, recursive: true);
             return exitCode;
         }
