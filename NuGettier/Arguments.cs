5dab45849d07710999c6df44109bc1ebe59bc86f
Chris Helmich <kagekirin@gmail.com>
2024-01-17 17:05:13 +0900

    feature (NuGettier): add argument for 'package file'

--- NuGettier/Arguments.cs
+++ NuGettier/Arguments.cs
@@ -9,4 +9,5 @@ public partial class Program
     private static Argument<string> PackageIdVersionArgument =
         new("packageIdVersion", "package.id[@version | @latest]");
     private static Argument<string> SearchTermArgument = new("searchTerm", "package id-ish or search term");
+    private static Argument<FileInfo> PackageFileArgument = new("packageFile", "pre-packed UPM package");
 }
