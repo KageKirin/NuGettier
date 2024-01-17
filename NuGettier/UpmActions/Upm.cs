15ff5874d968f1b25b52c43d49d93e2af078bb16
Chris Helmich <kagekirin@gmail.com>
2024-01-17 16:56:16 +0900

    feature (NuGettier): implement action 'upm publish-package'

--- NuGettier/UpmActions/Upm.cs
+++ NuGettier/UpmActions/Upm.cs
@@ -20,6 +20,7 @@ public partial class Program
             UpmUnpackCommand,
             UpmPublishCommand,
             UpmNpmrcCommand,
+            UpmPublishPackageCommand,
             AmalgamateCommand,
         };
 }
---
+++ NuGettier/UpmActions/UpmPublishPackage.cs
@@ -0,0 +1,76 @@
+using System;
+using System.Collections.Generic;
+using System.CommandLine;
+using System.CommandLine.Binding;
+using System.CommandLine.Invocation;
+using System.CommandLine.IO;
+using System.CommandLine.NamingConventionBinder;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using Xunit;
+
+namespace NuGettier;
+
+public partial class Program
+{
+    private static Command UpmPublishPackageCommand =>
+        new Command("publish-package", "publish a pre-packed UPM package")
+        {
+            PackageFileArgument,
+            TargetRegistryOption,
+            UpmDirectoryUrlOption,
+            UpmTokenOption,
+            UpmNpmrcOption,
+            UpmDryRunOption,
+            UpmTimeOutOption,
+            UpmPackageAccessLevel,
+        }.WithHandler(CommandHandler.Create(UpmPublishPackage));
+
+    private static async Task<int> UpmPublishPackage(
+        FileInfo packageFile,
+        Uri target,
+        string unity,
+        string? directory,
+        string? token,
+        string? npmrc,
+        bool dryRun,
+        int timeOut,
+        Upm.PackageAccessLevel access,
+        IConsole console,
+        CancellationToken cancellationToken
+    )
+    {
+        using var scope = Logger.TraceLocation().BeginScope(nameof(UpmPublishPackage));
+        Assert.NotNull(Configuration);
+        using var context = new Upm.Context(
+            configuration: Configuration!,
+            sources: Array.Empty<Uri>(),
+            minUnityVersion: (UpmUnityVersionOption as IValueDescriptor<string>).GetDefaultValue() as string
+                ?? string.Empty,
+            target: target,
+            repository: null,
+            directory: null,
+            console: console,
+            loggerFactory: MainLoggerFactory
+        );
+        var result = await context.PublishPackedUpmPackage(
+            packageFile: packageFile,
+            token: token,
+            npmrc: npmrc,
+            dryRun: dryRun,
+            timeOut: timeOut,
+            packageAccessLevel: access,
+            cancellationToken: cancellationToken
+        );
+
+        if (result != 0)
+        {
+            Logger.LogError("failed to publish UPM package {0}", packageFile.FullName);
+        }
+
+        Logger.LogTrace("exit {0} command handler with error {1}", nameof(UpmPublishPackage), result);
+        return result;
+    }
+}
