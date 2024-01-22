7ae84458dcaaf7dba7fec3d5004cb99375254ada
Christian Helmich <kagekirin@gmail.com>
2024-01-22 19:57:01 +0900

    fix (NuGettier.Amalgamate): pass arguments 'prereleaseSuffix' and 'buildmetaSuffix' to base implementation in Amalgamate.Context.PatchPackageJson() (fix/amalgamate-patch-package-json-base-call) [#167]
    - refactor (NuGettier.Amalgamate): pass argument 'prereleaseSuffix' to base implementation in Amalgamate.Context.PatchPackageJson()
    - refactor (NuGettier.Amalgamate): pass argument 'buildmetaSuffix' to base implementation in Amalgamate.Context.PatchPackageJson()
    
    - refactor (NuGettier.Amalgamate): pass argument 'prereleaseSuffix' to base implementation in Amalgamate.Context.PatchPackageJson()
    - refactor (NuGettier.Amalgamate): pass argument 'buildmetaSuffix' to base implementation in Amalgamate.Context.PatchPackageJson()

--- NuGettier.Amalgamate/Context/Utility.cs
+++ NuGettier.Amalgamate/Context/Utility.cs
@@ -27,7 +27,11 @@ public partial class Context
             .Where(d => GetPackageRule(d.Key).IsExcluded == false) //< filter: not 'excluded' dependencies are included)
             .ToDictionary(d => d.Key, d => d.Value);
 
-        var patchedPackageJson = base.PatchPackageJson(packageJson);
+        var patchedPackageJson = base.PatchPackageJson(
+            packageJson: packageJson,
+            prereleaseSuffix: prereleaseSuffix,
+            buildmetaSuffix: buildmetaSuffix
+        );
 
         patchedPackageJson.DisplayName += " amalgamated with its dependencies";
         patchedPackageJson.Description += "\n\nThis package also contains the following dependency assemblies:";
