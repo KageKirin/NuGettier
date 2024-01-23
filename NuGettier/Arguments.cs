ab78d17be154730584861c9ff3da543c13cf4e74
Chris Helmich <kagekirin@gmail.com>
2024-01-23 11:17:47 +0900

    feature (NuGettier): add input items argument

--- NuGettier/Arguments.cs
+++ NuGettier/Arguments.cs
@@ -10,4 +10,6 @@ public partial class Program
         new("packageIdVersion", "package.id[@version | @latest]");
     private static Argument<string> SearchTermArgument = new("searchTerm", "package id-ish or search term");
     private static Argument<FileInfo> PackageFileArgument = new("packageFile", "pre-packed UPM package");
+    private static Argument<IEnumerable<string>> InputItemsArgument =
+        new("inputItems", "input files and folders, and/or wildcard patterns");
 }
