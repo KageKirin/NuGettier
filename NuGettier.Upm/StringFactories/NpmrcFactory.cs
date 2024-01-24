7f855ec951ee6aa8470a6825c79bca7f9907118d
Christian Helmich <kagekirin@gmail.com>
2024-01-24 11:41:00 +0900

    refactor (NuGettier.Upm): implement and use NpmrcFactory to generate .npmrc contents in Upm.Context.GenerateNpmrc() (refactor/npmrc-factory) [#173]
    - feature (NuGettier.Upm): define interface INpmrcFactory
    - feature (NuGettier.Upm): implement interface INpmrcFactory as NpmrcFactory
    - refactor (NuGettier.Upm): use NpmrcFactory to generate .npmrc contents in Upm.Context.GenerateNpmrc()
    
    - feature (NuGettier.Upm): define interface INpmrcFactory
    - feature (NuGettier.Upm): implement interface INpmrcFactory as NpmrcFactory
    - refactor (NuGettier.Upm): use NpmrcFactory to generate .npmrc contents in Upm.Context.GenerateNpmrc()

---
+++ NuGettier.Upm/StringFactories/NpmrcFactory.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Text;
+
+namespace NuGettier.Upm;
+
+public interface INpmrcFactory
+{
+    string GenerateNpmrc(Uri registry, string authToken);
+}
+
+public class NpmrcFactory : INpmrcFactory
+{
+    public virtual string GenerateNpmrc(Uri registry, string authToken)
+    {
+        StringBuilder builder = new();
+
+        var uriScope = registry.Scope();
+        if (string.IsNullOrEmpty(uriScope))
+        {
+            // `//registry=${registry}/`
+            builder.AppendLine($"//registry={registry.ScopelessAbsoluteUri()}/");
+        }
+        else
+        {
+            // `//${uriScope}:registry=${registry}/`
+            builder.AppendLine($"//{uriScope}:registry={registry.ScopelessAbsoluteUri()}/");
+        }
+
+        // `//${host}/:_authToken=${authToken}`
+        builder.AppendLine($"//{registry.Host}/:_authToken={authToken}");
+
+        if (registry.Host != registry.Authority)
+        {
+            // `//${host}:${port}/:_authToken=${authToken}`
+            builder.AppendLine($"//{registry.Authority}/:_authToken={authToken}");
+        }
+
+        return builder.ToString();
+    }
+}
