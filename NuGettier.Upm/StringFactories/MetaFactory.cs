8b2242bc75f2172a8e1e0b601e54db5dc8a5e8fe
Chris Helmich <kagekirin@gmail.com>
2024-03-16 14:05:41 +0900

    refactor (NuGettier.Upm): use GuidFactory to generate Guid in MetaFactory.GenerateFileMeta()

--- NuGettier.Upm/StringFactories/MetaFactory.cs
+++ NuGettier.Upm/StringFactories/MetaFactory.cs
@@ -40,21 +40,14 @@ public class MetaFactory : IMetaFactory, IDisposable
     {
         using var scope = Logger.TraceLocation().BeginScope(this.__METHOD__());
 
-        var guid = new MetaGen.Guid(SeedHash, filename);
+        var guid = GuidFactory.GenerateGuid(filename);
         var metaTemplate = Handlebars.Compile(
             Path.GetExtension(filename).EndsWith(".dll")
                 ? EmbeddedAssetHelper.GetEmbeddedResourceString("NuGettier.Upm.Templates.assembly.meta")
                 : EmbeddedAssetHelper.GetEmbeddedResourceString("NuGettier.Upm.Templates.template.meta")
         );
         var metaContents = metaTemplate(new { guid = guid });
-        Logger.LogDebug(
-            "generated meta file for file {0} with seed {1} (hash {2:x}) (GUID: {3}):\n{4}",
-            filename,
-            Seed,
-            SeedHash,
-            guid,
-            metaContents
-        );
+        Logger.LogDebug("generated meta file for file {0} with (GUID: {1}):\n{2}", filename, guid, metaContents);
 
         return metaContents;
     }
