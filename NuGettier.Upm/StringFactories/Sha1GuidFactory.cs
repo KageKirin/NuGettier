6b1b8633e3fa3df6b3698772f2c5d5ac17ec14f3
Chris Helmich <kagekirin@gmail.com>
2024-03-16 20:26:02 +0900

    feature (NuGettier.Upm): implement IGuidFactory as Sha1GuidFactory

---
+++ NuGettier.Upm/StringFactories/Sha1GuidFactory.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Security.Cryptography;
+using System.Text;
+using Microsoft.Extensions.Logging;
+
+namespace NuGettier.Upm;
+
+public class Sha1GuidFactory : IGuidFactory, IDisposable
+{
+    protected readonly Microsoft.Extensions.Logging.ILogger Logger;
+
+    readonly string Seed = string.Empty;
+
+    public Sha1GuidFactory(Microsoft.Extensions.Logging.ILoggerFactory loggerFactory, string seed)
+    {
+        Logger = loggerFactory.CreateLogger<Sha1GuidFactory>();
+        Seed = seed;
+    }
+
+    public virtual Guid GenerateGuid(string value)
+    {
+        byte[] hash = SHA1.HashData(Encoding.Default.GetBytes($"{Seed}.{value}"));
+        byte[] limitedHash = new byte[16]; // Byte array for Guid must be exactly 16 bytes long.
+        Array.Copy(hash, 0, limitedHash, 0, limitedHash.Length);
+        Guid guid = new(limitedHash);
+        Logger.LogDebug("generated GUID {0:x} for {1} with seed {2}", guid.ToString("N"), value, Seed);
+
+        return guid;
+    }
+
+    public virtual void Dispose() { }
+}
