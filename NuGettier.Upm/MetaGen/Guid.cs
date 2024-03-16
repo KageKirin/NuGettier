39500a07e5eedb6367bc56a2816a2632d972c47d
Christian Helmich <kagekirin@gmail.com>
2024-03-16 14:36:08 +0900

    refactor (NuGettier.Upm): implement and use GuidFactory to generate Guids in IMetaFactory implementation (feature/guid-factory) [#189]
    - **feature (NuGettier.Upm): define interface IGuidFactory**
    - **feature (NuGettier.Upm): implement interface IGuidFactory as GuidFactory**
    - **feature (NuGettier.Upm): add IGuidFactory instance to MetaFactory**
    - **refactor (NuGettier.Upm): use GuidFactory to generate Guid in MetaFactory.GenerateFolderMeta()**
    - **refactor (NuGettier.Upm): use GuidFactory to generate Guid in MetaFactory.GenerateFileMeta()**
    - **refactor (NuGettier.Upm): remove Guid generation / hashing functionality from struct Guid**
    
    - **feature (NuGettier.Upm): define interface IGuidFactory**
    - **feature (NuGettier.Upm): implement interface IGuidFactory as GuidFactory**
    - **feature (NuGettier.Upm): add IGuidFactory instance to MetaFactory**
    - **refactor (NuGettier.Upm): use GuidFactory to generate Guid in MetaFactory.GenerateFolderMeta()**
    - **refactor (NuGettier.Upm): use GuidFactory to generate Guid in MetaFactory.GenerateFileMeta()**
    - **refactor (NuGettier.Upm): remove Guid generation / hashing functionality from struct Guid**

--- NuGettier.Upm/MetaGen/Guid.cs
+++ NuGettier.Upm/MetaGen/Guid.cs
@@ -1,34 +1,19 @@
 using System;
-using System.IO.Hashing;
-using System.Text;
-using Microsoft.Extensions.Logging;
 
 namespace NuGettier.Upm.MetaGen;
 
-struct Guid
+public struct Guid
 {
     public UInt128 hash;
-
-    public static UInt64 SeedHash(string seed)
-    {
-        XxHash64 xxHash = new();
-        xxHash.Append(Encoding.Default.GetBytes(seed));
-        return xxHash.GetCurrentHashAsUInt64();
-    }
 
     public Guid()
     {
         hash = default;
     }
 
-    public Guid(string seed, string value)
-        : this(seedHash: SeedHash(seed), value) { }
-
-    public Guid(ulong seedHash, string value)
+    public Guid(UInt128 hash)
     {
-        XxHash128 xxHash = new((long)seedHash);
-        xxHash.Append(Encoding.Default.GetBytes(value));
-        hash = xxHash.GetCurrentHashAsUInt128();
+        this.hash = hash;
     }
 
     public override readonly string ToString()
--- NuGettier.Upm/StringFactories/MetaFactory.cs
+++ NuGettier.Upm/StringFactories/MetaFactory.cs
@@ -13,33 +13,25 @@ public interface IMetaFactory
 public class MetaFactory : IMetaFactory, IDisposable
 {
     protected readonly Microsoft.Extensions.Logging.ILogger Logger;
-    protected string Seed;
-    protected ulong SeedHash;
+
+    protected readonly IGuidFactory GuidFactory;
 
     public MetaFactory(string seed, Microsoft.Extensions.Logging.ILoggerFactory loggerFactory)
     {
-        Seed = seed;
-        SeedHash = MetaGen.Guid.SeedHash(seed);
         Logger = loggerFactory.CreateLogger<MetaFactory>();
+        GuidFactory = new GuidFactory(loggerFactory, seed);
     }
 
     public virtual string GenerateFolderMeta(string dirname)
     {
         using var scope = Logger.TraceLocation().BeginScope(this.__METHOD__());
 
-        var guid = new MetaGen.Guid(SeedHash, dirname);
+        var guid = GuidFactory.GenerateGuid(dirname);
         var metaTemplate = Handlebars.Compile(
             EmbeddedAssetHelper.GetEmbeddedResourceString("NuGettier.Upm.Templates.folder.meta")
         );
         var metaContents = metaTemplate(new { guid = guid });
-        Logger.LogDebug(
-            "generated meta file for folder {0} with seed {1} (hash {2:x}) (GUID: {3}):\n{4}",
-            dirname,
-            Seed,
-            SeedHash,
-            guid,
-            metaContents
-        );
+        Logger.LogDebug("generated meta file for folder {0} with (GUID: {1}):\n{2}", dirname, guid, metaContents);
 
         return metaContents;
     }
@@ -48,21 +40,14 @@ public class MetaFactory : IMetaFactory, IDisposable
     {
         using var scope = Logger.TraceLocation().BeginScope(this.__METHOD__());
 
-        var guid = new MetaGen.Guid(SeedHash, filename);
+        var guid = GuidFactory.GenerateGuid(filename);
         var metaTemplate = Handlebars.Compile(
             Path.GetExtension(filename).EndsWith(".dll")
                 ? EmbeddedAssetHelper.GetEmbeddedResourceString("NuGettier.Upm.Templates.assembly.meta")
                 : EmbeddedAssetHelper.GetEmbeddedResourceString("NuGettier.Upm.Templates.template.meta")
         );
         var metaContents = metaTemplate(new { guid = guid });
-        Logger.LogDebug(
-            "generated meta file for file {0} with seed {1} (hash {2:x}) (GUID: {3}):\n{4}",
-            filename,
-            Seed,
-            SeedHash,
-            guid,
-            metaContents
-        );
+        Logger.LogDebug("generated meta file for file {0} with (GUID: {1}):\n{2}", filename, guid, metaContents);
 
         return metaContents;
     }
