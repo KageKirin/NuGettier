fbeb5118ef5d4743a6d5457c16f26d67ae5ec75d
Chris Helmich <kagekirin@gmail.com>
2024-03-18 00:16:41 +0900

    feature (NuGettier.Upm): implement Standart.Hash.xxHash3-based IGuidFactory as Uranium.XxHash3GuidFactory

---
+++ NuGettier.Upm/StringFactories/Uranium.XxHash3GuidFactory.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Text;
+using Microsoft.Extensions.Logging;
+using Standart.Hash.xxHash;
+using Xunit;
+
+namespace NuGettier.Upm.Uranium;
+
+public class XxHash3GuidFactory : IGuidFactory, IDisposable
+{
+    protected readonly Microsoft.Extensions.Logging.ILogger Logger;
+
+    readonly ulong SeedHash = 0;
+
+    public XxHash3GuidFactory(Microsoft.Extensions.Logging.ILoggerFactory loggerFactory, string seed)
+    {
+        Logger = loggerFactory.CreateLogger<XxHash3GuidFactory>();
+
+        byte[] seedBytes = Encoding.Default.GetBytes(seed);
+        SeedHash = xxHash3.ComputeHash(seedBytes, seedBytes.Length);
+    }
+
+    public virtual Guid GenerateGuid(string value)
+    {
+        byte[] valueBytes = Encoding.Default.GetBytes(value);
+        byte[] hashUpper = BitConverter.GetBytes(xxHash3.ComputeHash(valueBytes, valueBytes.Length, SeedHash));
+        byte[] hashLower = BitConverter.GetBytes(xxHash3.ComputeHash(hashUpper, hashUpper.Length, SeedHash));
+
+        Assert.Equal(8, hashUpper.Length);
+        Assert.Equal(8, hashLower.Length);
+
+        byte[] compositeHash = new byte[16]; // Byte array for Guid must be exactly 16 bytes long.
+        Array.Copy(hashUpper, 0, compositeHash, 0, hashUpper.Length);
+        Array.Copy(hashLower, 0, compositeHash, hashUpper.Length, hashLower.Length);
+        Guid guid = new(compositeHash);
+        Logger.LogDebug("generated GUID {0:x} for {1} with seed hash {2:x}", guid.ToString("N"), value, SeedHash);
+
+        return guid;
+    }
+
+    public virtual void Dispose() { }
+}
