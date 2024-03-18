4adb26cb9e119b08ff2766a4c337645b6ab2f25c
Christian Helmich <kagekirin@gmail.com>
2024-03-18 16:00:40 +0900

    refactor (NuGettier.Upm): split Guid.ToRfc4122Uuid() extension method into .ToRfc4122() and .ToUnityString() extension methods (refactor/split-guid-rfc411-extension-methods) [#197]
    - **refactor (NuGettier.Upm): split Guid.ToRfc4122Uuid() extension method into .ToRfc4122() and .ToUnityString() extension methods**
     reason: improve code understanding
     
    
    - **refactor (NuGettier.Upm): improve debug logs in MetaFactory**
     
    
    - **refactor (NuGettier.Upm): split Guid.ToRfc4122Uuid() extension method into .ToRfc4122() and .ToUnityString() extension methods**
     reason: improve code understanding
     
    
    - **refactor (NuGettier.Upm): improve debug logs in MetaFactory**

--- NuGettier.Upm/Utility/GuidExtension.cs
+++ NuGettier.Upm/Utility/GuidExtension.cs
@@ -4,12 +4,32 @@ namespace NuGettier.Upm;
 
 public static class GuidExtension
 {
-    public static string ToRfc4122Uuid(this Guid guid, byte version = 5)
+    /// <summary>
+    /// transforms the input Guid into a RFC-4122 UUID
+    /// </summary>
+    /// <see href="https://datatracker.ietf.org/doc/html/rfc4122#section-4.1.6" />
+    /// <param name="guid">a Guid</param>
+    /// <param name="version">indicates the RFC-4122 'version' parameter, which is '5' for SHA1 UUIDs</param>
+    /// <returns>the same Guid with a few bits changed</returns>
+    /// <remarks>
+    /// discovered this in Xoofx' UnityNuGet source code
+    /// https://github.com/xoofx/UnityNuGet
+    /// </remarks>
+    public static Guid ToRfc4122(this Guid guid, byte version = 5)
     {
         byte[] bytes = guid.ToByteArray();
         bytes[6] = (byte)((bytes[6] & 0x0F) | (version << 4));
         bytes[8] = (byte)((bytes[8] & 0x3F) | 0x80);
-        Guid uuid = new(bytes);
-        return uuid.ToString("N").ToLowerInvariant();
+        return new Guid(bytes);
+    }
+
+    /// <summary>
+    /// returns a Unity-compatible Guid string
+    /// </summary>
+    /// <param name="guid">a Guid</param>
+    /// <returns>lowercase hexadecimal character string without separators</returns>
+    public static string ToUnityString(this Guid guid)
+    {
+        return guid.ToString("N").ToLowerInvariant();
     }
 }
