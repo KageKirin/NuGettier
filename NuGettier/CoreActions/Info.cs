c70af66c03f3c85347143f4dd882d05b697bbce4
Chris Helmich <kagekirin@gmail.com>
2024-03-21 00:19:32 +0900

    refactor (NuGettier): adapt Info command to Core.Context.ctor() changes

--- NuGettier/CoreActions/Info.cs
+++ NuGettier/CoreActions/Info.cs
@@ -46,10 +46,11 @@ public partial class NuGettierService
         using var scope = Logger.TraceLocation().BeginScope(nameof(Info));
         Assert.NotNull(Configuration);
         using var context = new Core.Context(
-            configuration: Configuration!,
-            sources: sources,
+            configuration: Configuration,
+            loggerFactory: MainLoggerFactory,
+            logger: MainLoggerFactory.CreateLogger<Core.Context>(),
             console: console,
-            loggerFactory: MainLoggerFactory
+            sources: sources
         );
         var package = await context.GetPackageInformation(
             packageIdVersion: packageIdVersion,
