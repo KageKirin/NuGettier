d2e863561a2b4388913eb1af01a6b254e2fc02f5
Chris Helmich <kagekirin@gmail.com>
2024-03-25 00:42:03 +0900

    feature (NuGettier.Upm): add [GuidFormatterType] attribute to UnityGuidFormatter

--- NuGettier.Upm/Guid/UnityGuidFormatter.cs
+++ NuGettier.Upm/Guid/UnityGuidFormatter.cs
@@ -2,6 +2,7 @@ using System;
 
 namespace NuGettier.Upm;
 
+[GuidFormatterType(GuidFormat.Unity)]
 public class UnityGuidFormatter : IGuidFormatter
 {
     public virtual Guid Apply(Guid guid) => guid.ToUnityGUID();
