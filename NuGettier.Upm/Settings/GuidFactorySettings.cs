9c59c8cfb388efc621427f78b1d6aaf22f697760
Chris Helmich <kagekirin@gmail.com>
2024-03-25 00:53:42 +0900

    feature (NuGettier.Upm): define enum GuidFormat

--- NuGettier.Upm/Settings/GuidFactorySettings.cs
+++ NuGettier.Upm/Settings/GuidFactorySettings.cs
@@ -2,6 +2,16 @@ using System;
 
 namespace NuGettier.Upm;
 
+[Flags]
+public enum GuidFormat
+{
+    None = 0,
+    Unity = 1,
+    Rfc4122 = 2,
+    Unity_Rfc4122 = Unity | Rfc4122,
+    Mask = Unity | Rfc4122,
+}
+
 public sealed class GuidFactorySettings
 {
     public string Algorithm { get; set; } = string.Empty;
