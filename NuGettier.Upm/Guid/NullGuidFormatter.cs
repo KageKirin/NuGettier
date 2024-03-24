704976247aceacec606ba7cecc20a0d06996198c
Chris Helmich <kagekirin@gmail.com>
2024-03-25 00:41:47 +0900

    feature (NuGettier.Upm): add [GuidFormatterType] attribute to NullGuidFormatter

--- NuGettier.Upm/Guid/NullGuidFormatter.cs
+++ NuGettier.Upm/Guid/NullGuidFormatter.cs
@@ -2,6 +2,7 @@ using System;
 
 namespace NuGettier.Upm;
 
+[GuidFormatterType(GuidFormat.None)]
 public class NullGuidFormatter : IGuidFormatter
 {
     public virtual Guid Apply(Guid guid) => guid;
