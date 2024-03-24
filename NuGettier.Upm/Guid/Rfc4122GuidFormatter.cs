d901fc91dd789e7957c7bbcdaa1f9ee68ae7994a
Chris Helmich <kagekirin@gmail.com>
2024-03-25 00:42:16 +0900

    feature (NuGettier.Upm): add [GuidFormatterType] attribute to Rfc4122GuidFormatter

--- NuGettier.Upm/Guid/Rfc4122GuidFormatter.cs
+++ NuGettier.Upm/Guid/Rfc4122GuidFormatter.cs
@@ -2,6 +2,7 @@ using System;
 
 namespace NuGettier.Upm;
 
+[GuidFormatterType(GuidFormat.Rfc4122)]
 public class Rfc4122GuidFormatter : IGuidFormatter
 {
     public virtual Guid Apply(Guid guid) => guid.ToRfc4122();
